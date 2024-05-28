c32680b693ef55faadb2110eeb939196ef1dd946
Show Wai Yan <showwaiyan555@gmail.com>
2024-05-28 19:34:48 +0630

    Assigned HTML elements variables

---
+++ script.js
@@ -0,0 +1,7 @@
+// Global HTML Elements Variables
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d')
+
+const textArea = document.querySelector('textarea');
+
+const chooseFileBtn = document.querySelector('input["file"]');
