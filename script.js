185c3de8746a25909852ffed973d049ee75a139b
Show Wai Yan <showwaiyan555@gmail.com>
2024-05-28 19:36:59 +0630

    Assigned Image object variables

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const ctx = canvas.getContext('2d')
 const textArea = document.querySelector('textarea');
 
 const chooseFileBtn = document.querySelector('input["file"]');
+
+// Image Variables
+const img = new Image();
+let pixelArray; // Data to store image's each pixel's r,b,g values
