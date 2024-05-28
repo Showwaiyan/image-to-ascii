224e1bfc7ae25f1c31c76a5e42d2e377f4eae146
Show Wai Yan <showwaiyan555@gmail.com>
2024-05-28 19:43:08 +0630

    Added eventListener to ChooseFile button

--- script.js
+++ script.js
@@ -9,3 +9,6 @@ const chooseFileBtn = document.querySelector('input[type="file"]');
 // Image Variables
 const img = new Image();
 let pixelArray; // Data to store image's each pixel's r,b,g values
+
+// Image Source assigning
+chooseFileBtn.addEventListener('input',()=>img.src = chooseFileBtn.value);
