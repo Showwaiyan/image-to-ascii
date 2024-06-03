5bc23aa13b5c168737bde14429b1e99e3753c407
Show Wai Yan <showwaiyan555@gmail.com>
2024-06-04 00:22:21 +0630

    Finished processImgToAscii() function and remove ctx.fillStyle from global variable and add it and  ctx.font variable into processImgToAscii()

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // Global HTML Elements Variables
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-ctx.fillStyle = "white";
 
+// btn
 const chooseFileBtn = document.querySelector('input[type="file"]');
+
+// size radio input
+const sizeEle = document.querySelector('#size');
 
 // Image Variables
 const img = new Image();
@@ -15,9 +18,10 @@ let asciiImage="";
 // Image Source assigning
 chooseFileBtn.addEventListener('input',()=>{
     img.src = chooseFileBtn.value;
-    img.src = "/img/circle50.jpg";
+    img.src = "/img/Linux_logo.jpg";
     // Execution Instruction after loading img
     img.onload = function() {
+        canvas.width = 1080;
         canvas.height = canvas.width/(img.width/img.height) ;
         ctx.drawImage(img,0,0,canvas.width,canvas.height);
         pixelArray = ctx.getImageData(0,0,canvas.width,canvas.height).data;
@@ -29,11 +33,39 @@ function averageRGB(r,g,b) { return Math.trunc((r+g+b)/3)};
 
 // Substitute Character
 function substituteAsciiChar(avgRGB) {
-    const asciiArray = " _.,-=+:;cba!0123456789$W#@Ñ".split('');
+    const asciiArray = " _.,-=+:;cba!0123456789$W#@Ñ".split('').reverse();
     return asciiArray[Math.ceil(((asciiArray.length-1)*avgRGB)/255)];
 }
 
 // Processing Image to Ascii Character
 function processImgToAscii() {
+    // Clearing canvas to display ASCII result
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle="black";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = "white"; 
 
+    // Size for resolution
+    const size = Number(sizeEle.value);
+
+    for (let y=0; y<canvas.height; y+=size){
+        for (let x=0; x<canvas.width; x+=size) {
+            // Finding index and getting RGB value
+            const index = (y*canvas.width+x)*4;
+            const r = pixelArray[index];
+            const g = pixelArray[index+1];
+            const b = pixelArray[index+2];
+
+            // Getting Ascii value
+            const asciiChar = substituteAsciiChar(averageRGB(r,g,b));
+
+            // Fill text on canvas
+            ctx.font = `${size}px Courier`;
+            ctx.fillText(asciiChar,x,y);
+        }
+    }
 }
+
+// Testing
+document.querySelector('button').addEventListener('click',processImgToAscii);
+sizeEle.addEventListener('input',processImgToAscii);
